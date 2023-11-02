d6cd1527e667d535ad218cd84cb458c39ae06d76
Jack Song <xujiesong1993@gmail.com>
2023-11-02 19:55:14 +1100

    Added secret key

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3001
 
-app.get('/', (req, res) => res.send('Hello world'));
+app.get('/', (req, res) => {
+
+    const secretKey = process.env.BANK_SECRET_KEY;
+
+    res.send('Hello, this is my secret: ', secretKey)
+});
 
 app.listen(PORT, () => {
     console.log('App is up & running & listening')
